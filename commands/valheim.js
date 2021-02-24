fa73ef06cc661f77b4f499d5b0e6f582078b2c1f
Samuel Reinfelder <samreinf@gmx.net>
2021-02-24 15:13:14 +0100

    valheim command updated

--- commands/valheim.js
+++ commands/valheim.js
@@ -7,10 +7,12 @@ module.exports = {
 
         fetch(url)
             .then(response => response.json())
-            .then(data => console.log(message.channel.send(data.query + ":2456").then(
-                msg => {
-                    msg.delete({ timeout: 10000 });
-                }
-            )));
+            .then(
+                data => message.channel.send(data.query + ":2456").then(
+                    msg => {
+                        msg.delete({ timeout: 10000 });
+                    }
+                )
+            );
     },
 };
