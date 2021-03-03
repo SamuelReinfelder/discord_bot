069790854b8060796aa07abbc55770ca44bfabda
Samuel Reinfelder <samreinf@gmx.net>
2021-03-03 10:56:20 +0100

    After Dns change not longer needed

--- commands/valheim.js
+++ commands/valheim.js
@@ -5,14 +5,21 @@ module.exports = {
         const fetch = require('node-fetch');
         let url = "http://ip-api.com/json/" + config.valheim;
 
-        fetch(url)
-            .then(response => response.json())
-            .then(
-                data => message.channel.send(data.query + ":2456").then(
-                    msg => {
-                        msg.delete({ timeout: 10000 });
-                    }
-                )
-            );
+        // fetch(url)
+        //     .then(response => response.json())
+        //     .then(
+        //         data => message.channel.send(data.query + ":2456").then(
+        //             msg => {
+        //                 msg.delete({ timeout: 10000 });
+        //             }
+        //         )
+        //     );
+
+        message.channel.send("ladebeck.de").then(
+            msg => {
+                msg.delete({ timeout: 10000 });
+            }
+        )
+
     },
 };
